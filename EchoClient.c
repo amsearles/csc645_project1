58d587406e9d283e0af08046aa7573e99922f2a3
amsearles <asearles@mail.sfsu.edu>
2018-10-01 17:03:18 -0700

    Update EchoClient.c

--- EchoClient.c
+++ EchoClient.c
@@ -1,11 +1,11 @@
-#include <stdio.h> /* for printf() and fprintf() */
-#include <sys/socket.h> /* for socket(), connect(), send(), and recv() */
-#include <arpa/inet.h> /* for sockaddr_in and inet_addr() */
-#include <stdlib.h> /* for atoi() */
-#include <string.h> /* for memset() */
-#include <unistd.h> /* for close() */
+#include <stdio.h> 
+#include <sys/socket.h> 
+#include <arpa/inet.h> 
+#include <stdlib.h> 
+#include <string.h> 
+#include <unistd.h> 
 
-#define RCVBUFSIZE 1000 /* Size of receive buffer */
+#define RCVBUFSIZE 1000 
 #define MAXPENDING 5
 void DieWithError(char *errorMessage)
 {
